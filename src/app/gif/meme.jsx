52cc756946b64da74ea7bcf7acdc503fa2d164a3
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-12-15 15:40:29 +0530

    done

--- src/app/gif/meme.jsx
+++ src/app/gif/meme.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import React from 'react'
 
-const Meme = ({e,next,page,setNext,setPage}) => {
+const Meme = ({e,next,page,setNext,setPage,home,setShow,setsearch}) => {
     return (
-        <div className='flex items-center justify-center flex-col'>
+        <div className='flex gap-3 items-center justify-center flex-col'>
             <div className='flex flex-wrap gap-3 items-center justify-center p-10'>
                 <div className={`imgholder p-5 flex items-center justify-center ${next == page ? "border-blue-600 border-4" : ""} bg-slate-300 overflow-hidden rounded-xl`}>
                     <Image src={e[page].images.original.url} alt='asdf' width={500} height={500}></Image>
@@ -15,7 +15,7 @@ const Meme = ({e,next,page,setNext,setPage}) => {
                     <Image src={e[page + 2].images.original.url} alt='asdf' width={500} height={500}></Image>
                 </div>}
             </div>
-            <div className='flex flex-col items-center justify-center '>
+            <div className='flex gap-3 p-3 flex-col items-center justify-center '>
                 <div className='flex gap-2 items-center w-5/6 justify-center'>
                     <button className='p-2 rounded bg-white transition-all hover:bg-blue-700 hover:text-white w-32 h-14 text-xl font-bold' onClick={() => { setNext(next == 0 ? e.length - 1 : next - 1); let k = (next == 0 ? e.length - 1 : next - 1) / 3; setPage(3 * Math.floor(k)); }}>Previous</button>
                     <h2 className={`p-3 text-xl font-bold ${next == page ? "bg-red-200 border-b-4" : ""} border-red-500`}>{page}</h2>
@@ -24,6 +24,7 @@ const Meme = ({e,next,page,setNext,setPage}) => {
                     <button className='p-2 rounded bg-white hover:bg-blue-700 hover:text-white  w-32 h-14 text-xl font-bold' onClick={() => { setNext(next == e.length - 1 ? 0 : next + 1); let k = (next == e.length - 1 ? 0 : next + 1) / 3; setPage(3 * Math.floor(k)); }}>Next</button>
                 </div>
             </div>
+            {home && <button className='p-2 rounded text-white bg-black hover:bg-blue-700 hover:text-white w-32 h-14 text-xl font-bold' onClick={() => {setPage(0); setNext(0); setShow([]); setsearch("")}}>Home</button>}
         </div>
     )
 }
--- src/app/gif/next.jsx
+++ src/app/gif/next.jsx
@@ -42,15 +42,15 @@ const Next = ({ e }) => {
     return (
         <div className="bg-white mainBox relative rounded-xl p-10">
             <div className='flex z-40 flex-wrap gap-2 justify-center items-center'>
-                <div className=' bg-slate-200 flex gap-3 w-5/6 rounded-xl p-3'>
+                <div className=' bg-slate-200 flex gap-3 md:w-full lg:w-5/6 rounded-xl p-3'>
                     <Image src={"/Search icon.png"} alt='search' width={30} height={30}></Image>
                     <input value={search} onChange={e => setsearch(e.target.value)} className='bg-slate-200 font-bold text-xl w-5/6 focus:outline-none focus:border-transparent px-2' placeholder='Search for stickers' type="text" />
                 </div>
                 <button className='p-2 rounded-xl bg-black text-white hover:bg-blue-700 hover:text-white w-32 h-14 text-xl font-bold' onClick={Search}>Search</button>
                 <button className='p-2 rounded-xl bg-black text-white hover:bg-blue-700 hover:text-white w-32 h-14 text-xl font-bold' onClick={()=>{Cookies.remove('Token'); router.replace("/");}}>Log out</button>
             </div>
-            {show.length == 0 && !loading && <Meme e={e} next={next} page={page} setNext={setNext} setPage={setPage}/>}
-            {show.length != 0 && !loading && <Meme e={show} next={next} page={page} setNext={setNext} setPage={setPage}/>}
+            {show.length == 0 && !loading && <Meme e={e} next={next} page={page} setNext={setNext} setPage={setPage} home={false} setShow={setShow} setsearch={setsearch}/>}
+            {show.length != 0 && !loading && <Meme e={show} next={next} page={page} setNext={setNext} setPage={setPage} home={true} setShow={setShow} setsearch={setsearch}/>}
             {loading && <div className='flex items-center justify-center h-full'>
                 <div>
                     <Image className='infinite-rotate' src={"/Spinner.png"} width={50} height={50} alt='loading...'></Image>
